3cb296875de4b7d7607226897522f162d1afb24e
weiqiang <weiqiang@doumeng>
2022-12-01 20:20:42 +0800

    重构中 ...

--- pkg/logs/log.go
+++ pkg/logs/log.go
@@ -1,4 +1,4 @@
-package model
+package logs
 
 import (
 	"fmt"
@@ -7,12 +7,12 @@ import (
 )
 
 // access log: $data $statusCode $receiver $status $grade $alertname $[alertSummary|msg]
-func init(){
+func init() {
 	file, err := os.OpenFile("log/infra-prometheus-webhook.log", os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0755)
 	if err != nil {
 		fmt.Print(err.Error())
 		os.Exit(1)
 	}
-	log.SetFlags(log.Ldate|log.Lmicroseconds|log.LUTC)
+	log.SetFlags(log.Ldate | log.Lmicroseconds | log.LUTC)
 	log.SetOutput(file)
 }
